7715cbf42af2881e9ad1f3e14be7f7d42f31f5b9
Nikshitha Shetty <nikshitha.s@sonata-software.com>
2023-11-21 18:15:56 +0530

    Update on pdp

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getProductById } from "./api-helper/frontend/util.js";
+import { getProductById } from "../../container/src/api-helper/frontend/util.js";
 import ProductDetails from "./component/product/ProductDetail.js"
 export default function Root(props) {
   const productId = props.productId; // Access productId as a prop
